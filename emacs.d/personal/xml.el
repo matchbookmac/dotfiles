29e9c3baf354868259e2442010f6ca12bb7537da
Ian MacDonald <imacdonald@rumblefish.com>
2016-02-02 09:48:02 -0800

    master: print aliases alias, xml indentation format for emacs

---
+++ emacs.d/personal/xml.el
@@ -0,0 +1,10 @@
+;;; package --- Summary
+;; Set indent for xml documents to 2 spaces
+
+;;; Commentary:
+;; The module prelude-xml.el sets the indet for xml docs
+;; to 4 spaces, I prefer 2.
+
+;;; Code:
+(setq nxml-child-indent 2)
+(setq nxml-attribute-indent 2)
