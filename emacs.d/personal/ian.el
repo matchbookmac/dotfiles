dea45576f65e42796ea9266c1cb1703322c485df
Ian MacDonald <imacdonald@rumblefish.com>
2016-04-22 15:38:54 -0700

    master: Updates

--- emacs.d/personal/ian.el
+++ emacs.d/personal/ian.el
@@ -1,15 +1,24 @@
-;;; Commentary
+;;; package --- Summary
+;; Personal config for Emacs prelude
 
-;; disable emacs menubar
+;;; Commentary:
+
+;;; code:
+
+;; disable Emacs menubar
 (menu-bar-mode -1)
 
 ;; use guru mode to disable noob keys
 (setq guru-warn-only nil)
 
 ;; require the silver searcher
-(prelude-require-package 'ag)
+(prelude-require-packages '(dash-at-point ag))
 
 ;; keybindings
 (global-set-key (kbd "C-c ;") 'ian/comment-or-uncomment-region-or-line)
+(global-set-key (kbd "C-c C-d d") 'dash-at-point)
+(global-set-key (kbd "C-c C-d e") 'dash-at-point-with-docset)
 
 (setq whitespace-line-column 120) ;; limit line length
+
+;;; ian.el ends here
